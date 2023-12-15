14ea097cfc57740616f1531c648a8ce96fcc17cb
Danny North <iamdnorth@gmail.com>
2023-12-15 01:54:09 -0700

    Add the calculateMaxDepth function with tests and using dynamic programming/memoization in order to improve time complexity significantly

--- src/tests/graphDetails.spec.js
+++ src/tests/graphDetails.spec.js
@@ -1,4 +1,4 @@
-import { calculateRootCount } from "../utils/graphDetails"
+import { calculateRootCount, calculateMaxDepth } from "../utils/graphDetails"
 
 describe('calculateRootCount', () => {
   describe('when there are only 2 layers', () => {
@@ -85,9 +85,85 @@ describe('calculateRootCount', () => {
 })
 
 describe('calculateMaxDepth', () => {
-  describe('when something', () => {
-    it('should do something', () => {
-      expect(true).toBeTruthy()
+  describe('when there are only 2 layers', () => {
+    const tasks = [{ id: "1" }, { id: "2" }]
+    const dependencies = [{
+      "predecessor_id": "1",
+      "successor_id": "2"
+    }]
+
+    const actualResult = calculateMaxDepth(tasks, dependencies)
+
+    it('the maximum depth should be 2', () => {
+      const expectedResult = 2
+
+      expect(actualResult).toEqual(expectedResult)
+    })
+  })
+
+  describe('when there are more than 2 layers', () => {
+    const tasks = [
+      { "id": "1", "name": "Root Task 1" },
+      { "id": "2", "name": "Root Task 2" },
+      { "id": "3", "name": "Mid Layer Task 1" },
+      { "id": "4", "name": "Mid Layer Task 2" },
+      { "id": "5", "name": "Mid Layer Task 3" },
+      { "id": "6", "name": "Leaf Task 1" },
+      { "id": "7", "name": "Leaf Task 2" }
+    ]
+    
+    const dependencies = [
+      { "predecessor_id": "1", "successor_id": "3" },
+      { "predecessor_id": "2", "successor_id": "3" },
+      { "predecessor_id": "2", "successor_id": "4" },
+      { "predecessor_id": "1", "successor_id": "5" },
+      { "predecessor_id": "3", "successor_id": "6" },
+      { "predecessor_id": "4", "successor_id": "6" },
+      { "predecessor_id": "5", "successor_id": "7" }
+    ]
+    
+
+    const actualResult = calculateMaxDepth(tasks, dependencies)
+
+    it('the maximum depth should be 3', () => {
+      const expectedResult = 3
+
+      expect(actualResult).toEqual(expectedResult)
+    })
+  })
+
+  describe('when some tasks are neither a successor nor a predecessor', () => {
+    const tasks = [
+      { "id": "1", "name": "Root Task 1" },
+      { "id": "2", "name": "Root Task 2" },
+      { "id": "3", "name": "Mid Layer Task 1" },
+      { "id": "4", "name": "Mid Layer Task 2" },
+      { "id": "5", "name": "Mid Layer Task 3" },
+      { "id": "6", "name": "Leaf Task 1" },
+      { "id": "7", "name": "Leaf Task 2" },
+      { "id": "8", "name": "Isolated Task 1" },
+      { "id": "9", "name": "Isolated Task 2" },
+      { "id": "10", "name": "Isolated Task 3" }
+    ];
+    
+    
+    const dependencies = [
+      { "predecessor_id": "1", "successor_id": "3" },
+      { "predecessor_id": "2", "successor_id": "3" },
+      { "predecessor_id": "2", "successor_id": "4" },
+      { "predecessor_id": "1", "successor_id": "5" },
+      { "predecessor_id": "3", "successor_id": "6" },
+      { "predecessor_id": "4", "successor_id": "6" },
+      { "predecessor_id": "5", "successor_id": "7" }
+    ]
+    
+
+    const actualResult = calculateMaxDepth(tasks, dependencies)
+
+    it('the maximum depth should include isolated tasks as depth 1', () => {
+      const expectedResult = 3 // The depth of the main graph
+
+      expect(actualResult).toEqual(expectedResult)
     })
   })
 })
--- src/utils/graphDetails.js
+++ src/utils/graphDetails.js
@@ -9,5 +9,27 @@ export const calculateRootCount = (tasks, dependencies) => {
 };
 
 export const calculateMaxDepth = (tasks, dependencies) => {
-  return tasks.length + dependencies.length
-}
+  const depsOrganized = dependencies.reduce((acc, dependency) => {
+    if (!acc.graph[dependency.predecessor_id]) {
+      acc.graph[dependency.predecessor_id] = [];
+    }
+    acc.graph[dependency.predecessor_id].push(dependency.successor_id);
+    return acc;
+  }, { graph: {} });
+
+  const roots = getRoots(tasks, dependencies);
+  const depthMemo = {};
+
+  const dfs = (node) => {
+    if (depthMemo[node] !== undefined) return depthMemo[node];
+    if (!depsOrganized.graph[node] || depsOrganized.graph[node].length === 0) {
+      depthMemo[node] = 1;
+      return 1;
+    }
+    const maxDepth = Math.max(...depsOrganized.graph[node].map(successor => dfs(successor))) + 1;
+    depthMemo[node] = maxDepth;
+    return maxDepth;
+  };
+
+  return Math.max(...roots.map(root => dfs(root)));
+};
