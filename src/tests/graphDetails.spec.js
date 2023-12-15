a927cd954564fd02b50afd9c68ca837fe62167bb
Danny North <iamdnorth@gmail.com>
2023-12-15 01:06:08 -0700

    add calculateRootCount code with tests

--- src/tests/graphDetails.spec.js
+++ src/tests/graphDetails.spec.js
@@ -1,7 +1,50 @@
+import { calculateRootCount } from "../utils/graphDetails"
+
 describe('calculateRootCount', () => {
-  describe('when something', () => {
-    it('should do something', () => {
-      expect(true).toBeTruthy()
+  describe('when there are only 2 layers', () => {
+    const tasks = [{ id: "1" }, { id: "2" }]
+    const dependencies = [{
+      "predecessor_id": "1",
+      "successor_id": "2"
+    }]
+
+    const actualResult = calculateRootCount(tasks, dependencies)
+
+    it('only nodes on the first layer should be counted as a root', () => {
+      const expectedResult = 1
+
+      expect(actualResult).toEqual(expectedResult)
+    })
+  })
+
+  describe('when there are more than 2 layers', () => {
+    const tasks = [
+      { "id": "1", "name": "Root Task 1" },
+      { "id": "2", "name": "Root Task 2" },
+      { "id": "3", "name": "Mid Layer Task 1" },
+      { "id": "4", "name": "Mid Layer Task 2" },
+      { "id": "5", "name": "Mid Layer Task 3" },
+      { "id": "6", "name": "Leaf Task 1" },
+      { "id": "7", "name": "Leaf Task 2" }
+    ]
+    
+    const dependencies = [
+      { "predecessor_id": "1", "successor_id": "3" },
+      { "predecessor_id": "2", "successor_id": "3" },
+      { "predecessor_id": "2", "successor_id": "4" },
+      { "predecessor_id": "1", "successor_id": "5" },
+      { "predecessor_id": "3", "successor_id": "6" },
+      { "predecessor_id": "4", "successor_id": "6" },
+      { "predecessor_id": "5", "successor_id": "7" }
+    ]
+    
+
+    const actualResult = calculateRootCount(tasks, dependencies)
+
+    it('only nodes on the first layer should be counted as a root', () => {
+      const expectedResult = 2
+
+      expect(actualResult).toEqual(expectedResult)
     })
   })
 })
--- src/utils/graphDetails.js
+++ src/utils/graphDetails.js
@@ -1,5 +1,17 @@
-export const calculateRootCount = (tasks, dependencies) => {
-  return tasks.length + dependencies.length
+// O(n + m)
+export const calculateRootCount = (dependencies) => {
+  //n - number of dependencies
+  const depsOrganized = dependencies.reduce((acc, dependency) => {
+    return {
+      predecessors: acc.predecessors.add(dependency.predecessor_id),
+      successors: acc.successors.add(dependency.successor_id),
+    }
+  }, { predecessors: new Set(), successors: new Set()})
+
+  //m - number of unique predecessors
+  const setDifference = new Set(Array.from(depsOrganized.predecessors).filter(item => !depsOrganized.successors.has(item)))
+
+  return setDifference.size
 }
 
 export const calculateMaxDepth = (tasks, dependencies) => {
